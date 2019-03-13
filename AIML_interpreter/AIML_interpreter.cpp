6d423ea65bc8451d78c96857d99510d8d330f873
Kyle Klouzal <kylek@KKlouzal-Laptop.localdomain>
2019-03-13 11:12:23 -0700

    Add project files.

---
+++ AIML_interpreter/AIML_interpreter.cpp
@@ -0,0 +1,258 @@
+// AIML_interpreter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <utility>
+#include <queue>
+#include <vector>
+#include <list>
+#include <array>
+#include <filesystem>
+#include <functional>
+#include <iostream>
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+
+namespace AIML
+{
+	namespace fs = std::experimental::filesystem;
+
+	class Category
+	{
+	public:
+		std::string Pattern;	// Input pattern
+		std::string Topic;		// Topic of this category
+		std::string That;		// Context limiter for this category
+		bool Srai;				// Rematch using this pattern
+		std::vector<std::list<std::string*>> Templates;	// Response templates
+
+		Category(std::string UsePattern, std::string UseTopic) : Pattern(UsePattern), Topic(UseTopic), Srai(false)
+		{}
+
+		~Category() {
+			for (auto Template : Templates) {
+				for (auto Piece : Template) {
+					//delete Piece;
+				}
+			}
+		}
+
+		//	So we can visually debug the state of our memory
+		void PrintData() {
+			std::cout << std::endl << "Category Data" << std::endl;
+			std::cout << "\tPattern: " << Pattern << std::endl;
+			std::cout << "\tTopic: " << Topic << std::endl;
+			std::cout << "\tThat: " << That << std::endl;
+			std::cout << "\tSrai: " << Srai << std::endl;
+			std::cout << "\tTemplates: " << std::endl;
+			unsigned int TemplateNum = 0;
+			for (auto Template : Templates) {
+				std::cout << "\t\t[" << TemplateNum << "]:" << std::endl;
+				TemplateNum++;
+				unsigned int PieceNum = 0;
+				for (auto Piece : Template) {
+					std::cout << "\t\t\t[" << PieceNum << "]: " << *Piece << std::endl;
+					PieceNum++;
+				}
+			}
+		}
+
+		void SetSRAI() { Srai = true; }
+
+		void SetThat(std::string UseThat) { That = UseThat; }
+	};
+
+	class Bot
+	{
+		std::queue<rapidxml::file<>> XML;
+		std::queue<rapidxml::xml_document<>> Documents;
+	public:
+		std::string Topic;	// Current topic
+		std::string That;	// Current context
+		std::array<std::string, 8> Stars; // Current <star/> inputs
+		std::vector<Category> Category_List;
+
+		void DebugCategories() {
+			for (auto Category : Category_List) {
+				Category.PrintData();
+			}
+		}
+
+		void DebugStars() {
+			Stars[0] = "Quick";
+			Stars[1] = "Brown";
+			Stars[2] = "Fox";
+			Stars[3] = "Jumps";
+			Stars[4] = "Over";
+			Stars[5] = "The";
+			Stars[6] = "Lazy";
+			Stars[7] = "Dog";
+
+		}
+
+		//	Recursively walk through a TEMPLATE constructing response templates for an AIML::Category
+		void WalkTemplate(const rapidxml::xml_node<>* node) {
+
+			const rapidxml::node_type t = node->type();
+			switch (t) {
+			case rapidxml::node_element:
+			{
+				//	Encountered <random>
+				if (strcmp(node->name(), "random") == 0) {
+					//	Set flag for use of random responses?
+					//	This could eliminate the use of double nested arrays
+					//		for single response patterns.
+				}
+				//	Encountered <li>
+				else if (strcmp(node->name(), "li") == 0) {
+					//	Create a new Response List at the back of Templates
+					Category_List.back().Templates.emplace_back(std::list<std::string*>());
+				}
+				//	Encountered <srai>
+				else if (strcmp(node->name(), "srai") == 0) {
+					//	Enable this category to use symbolic reduction (sari)
+					Category_List.back().SetSRAI();
+				}
+				else if (strcmp(node->name(), "star") == 0) {
+					//	Insert reference to appropriate <star/> value
+					Category_List.back().Templates.back().push_back(&Stars[0]);
+				}
+				else {
+					printf("<%s>\n", node->name());
+				}
+				for (auto a = node->first_attribute(); a; a = a->next_attribute()) {
+					printf(" %s", a->name());
+					printf("='%s'", a->value());
+				}
+
+				for (auto n = node->first_node(); n; n = n->next_sibling()) {
+					WalkTemplate(n);
+				}
+			}
+			break;
+
+			case rapidxml::node_data:
+				//	Add this data to the end of the current template
+					if (Category_List.back().Templates.empty()) {
+						Category_List.back().Templates.emplace_back(std::list<std::string*>(1, new std::string(node->value())));
+					}
+					else {
+						Category_List.back().Templates.back().emplace_back(new std::string(node->value()));
+					}
+					//printf("DATA:[%s]\n", node->value());
+				break;
+
+			default:
+				printf("NODE-TYPE:%d\n", t);
+				break;
+			}
+		}
+
+		void ParseCategories(rapidxml::xml_node<>* Root_Node, std::string UseTopic = "") {
+			for (rapidxml::xml_node<>* Category_Node = Root_Node->first_node("category"); Category_Node; Category_Node = Category_Node->next_sibling())
+			{
+				//	Dissect PATTERN
+				rapidxml::xml_node<>* Pattern_Node = Category_Node->first_node("pattern");
+				if (Pattern_Node == NULL) {
+					std::cout << "Skipping Malformed Category: No PATTERN" << std::endl;
+					continue;
+				}
+				std::string Pattern = Pattern_Node->value();
+				//std::cout << Pattern << std::endl;
+				//	Dissect TEMPLATE
+				rapidxml::xml_node<>* Template_Node = Category_Node->first_node("template");
+				if (Template_Node == NULL) {
+					std::cout << "Skipping Malformed Category: No TEMPLATE" << std::endl;
+					continue;
+				}
+				//	Start constructing a new CATEGORY
+				Category_List.emplace_back(Category(Pattern, UseTopic));
+				for (auto Node = Template_Node->first_node(); Node; Node = Node->next_sibling()) {
+					WalkTemplate(Node);
+				}
+				//	Dissect THAT
+				rapidxml::xml_node<>* That_Node = Category_Node->first_node("that");
+				if (That_Node != NULL) {
+					std::string That = That_Node->value();
+					Category_List.back().SetThat(That);
+				}
+				std::cout << std::endl;
+			}
+		}
+
+		//	Default debugging values for the Stars array.
+		Bot() : Stars({ "*1*","*2*","*3*","*4*","*5*","*6*","*7*","*8*" })
+		{
+			fs::path AIML_Folder = fs::current_path().append("AIML");
+			std::cout << AIML_Folder << std::endl;
+			if (fs::exists(AIML_Folder) && fs::is_directory(AIML_Folder)) {
+				std::cout << "Found AIML Folder" << std::endl;
+				for (const auto& entry : fs::directory_iterator(AIML_Folder))
+				{
+					auto filename = entry.path().filename();
+					if (fs::is_directory(entry.status()))
+					{
+						std::cout << "Unable to load nested folders (yet)" << std::endl;
+					}
+					else if (fs::is_regular_file(entry.status())) {
+						std::cout << "Loading (" << filename << ").." << std::endl;
+						XML.emplace(entry.path().string().c_str());
+						Documents.emplace();
+						try {
+							//	Parse the raw XML data into an XML Document object
+							Documents.back().parse<0 | rapidxml::parse_normalize_whitespace>(XML.back().data());
+							//	Iterate through the XML extracting C++ objects from the AIML
+							rapidxml::xml_node<>* Root_Node = Documents.back().first_node("aiml");
+							for (rapidxml::xml_node<>* Topic_Node = Root_Node->first_node("topic"); Topic_Node; Topic_Node = Topic_Node->next_sibling())
+							{
+								if (strcmp(Topic_Node->name(), "topic") == 0) {
+									rapidxml::xml_attribute<>* Topic_Name = Topic_Node->first_attribute("name");
+									if (Topic_Name == NULL) {
+										std::cout << "Skipping Malformed Topic: No NAME" << std::endl;
+										continue;
+									}
+									ParseCategories(Topic_Node, Topic_Name->value());
+								}
+							}
+							ParseCategories(Root_Node);
+						}
+						catch (const rapidxml::parse_error & e) {
+							std::cout << "\tXML Error: " << e.what() << " - " << e.where<char>() << std::endl;
+						}
+					}
+				}
+			}
+		}
+
+		~Bot() {
+			while (!Documents.empty()) {
+				Documents.pop();
+			}
+			while (!XML.empty()) {
+				XML.pop();
+			}
+		}
+	};
+}
+
+int main()
+{
+	AIML::Bot MyBot;
+	std::system("PAUSE");
+	MyBot.DebugCategories();
+	std::system("PAUSE");
+	MyBot.DebugStars();
+	MyBot.DebugCategories();
+	std::system("PAUSE");
+    //std::cout << "Hello World!\n"; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
